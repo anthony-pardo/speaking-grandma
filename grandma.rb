96cb535a8df1e651671a55abdeae2b59b6f0f489
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-11 21:32:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(str)
   elsif str == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else 
-    return "NO! NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
